18aff28dc3f2483dab648482449cdc85e2bed75f
ariam0019 <ariamenamorado00@gmail.com>
2022-12-01 21:52:21 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio2/Ejercicio2/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btninformacion_Clicked(object sender, EventArgs e)
+        {
+            var vari = new Model.Variables
+            {
+                Nombre = txtnombre.Text,
+                Apellidos = txtapellidos.Text,
+                Edad = Convert.ToInt32(txtedad.Text), 
+                Correo= txtcorreo.Text,
+                EnviarNombre = txtnombre.Text,
+                EnviarApellido = txtapellidos.Text,
+                EnviarEdad = Convert.ToInt32(txtedad.Text),
+                EnviarCorreo = txtcorreo.Text
+            };
+            var ma = new Views.Agregar();
+            ma.BindingContext = vari;
+            await Navigation.PushModalAsync(ma);
+        }
+    }
+}
---
+++ Ejercicio2/Ejercicio2/Model/Variables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio2.Model
+{
+    public class Variables
+    {
+        public string Nombre { get; set; }  
+        public string Apellidos { get; set; }   
+        public int Edad { get; set; }
+        public string Correo { get; set; }
+        public  string Agregar { get; set; }
+        public string EnviarNombre { get; set; }
+        public string EnviarApellido { get; set; }
+        public int EnviarEdad { get; set; }
+        public string EnviarCorreo { get; set; }
+    }
+}
